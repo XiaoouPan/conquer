1cacb8c5c9ccb8b30111434b003d6039e6d02094
Xiaoou Pan <xip024@ucsd.edu>
2020-04-04 20:20:32 -0700

    Update reference

--- R/conquer-package.R
+++ R/conquer-package.R
@@ -6,7 +6,9 @@
 #' @references Barzilai, J. and Borwein, J. M. (1988). Two-point step size gradient methods. IMA J. Numer. Anal. 8 141–148.
 #' @references Fernandes, M., Guerre, E. and Horta, E. (2019). Smoothing quantile regressions. J. Bus. Econ. Statist., in press.
 #' @references He, X., Pan, X., Tan, K. M., and Zhou, W.-X. (2020). Smoothed quantile regression: Fast computation and bootstrap inference. Preprint.
+#' @references Horowitz, J. L. (1998). Bootstrap methods for median regression models. Econometrica 66 1327–1351.
 #' @references Koenker, R. (2005). Quantile Regression. Cambridge University Press, Cambridge.
 #' @references Koenker, R. Package "quantreg".
 #' @references Koenker, R. and Bassett, G. (1978). Regression quantiles. Econometrica 46 33-50.
+#' @references Pan, X. and Zhou, W.-X. (2020). Multiplier bootstrap for quantile regression: Non-asymptotic theory under random design. Information and Inference: A Journal of the IMA, to appear.
 NULL
