bfbe5ca235f19c12eb987924ac5e14b438caa566
Xiaoou Pan <xip024@ucsd.edu>
2020-07-05 11:52:03 -0700

    One-step conquer

--- src/smqr.cpp
+++ src/smqr.cpp
@@ -767,3 +767,54 @@ Rcpp::List smqrHoro(const arma::mat& Z, const arma::vec& Y, const double tau = 0
   }
   return Rcpp::List::create(Rcpp::Named("coeff") = betaNew, Rcpp::Named("ite") = ite);
 }
+
+// [[Rcpp::export]]
+Rcpp::List osSmqrGauss(const arma::mat& X, arma::vec Y, const double tau = 0.5, double h = 0.0, const double constTau = 1.345, 
+                       const double tol = 0.0001, const int iteMax = 5000) {
+  const int n = X.n_rows;
+  const int p = X.n_cols;
+  if (h <= 0.05) {
+    h = std::max(std::pow((std::log(n) + p) / n, 0.4), 0.05);
+  }
+  const double n1 = 1.0 / n;
+  const double h1 = 1.0 / h;
+  arma::rowvec mx = arma::mean(X, 0);
+  arma::vec sx = arma::stddev(X, 0, 0).t();
+  arma::mat Z = arma::join_rows(arma::ones(n), standardize(X, mx, sx, p));
+  double my = arma::mean(Y);
+  Y -= my;
+  arma::vec der(n);
+  arma::vec gradOld(p + 1), gradNew(p + 1);
+  arma::vec beta = huberReg(Z, Y, der, gradOld, gradNew, n, p, n1, tol, constTau, iteMax);
+  arma::vec quant = {tau};
+  beta(0) = arma::as_scalar(arma::quantile(Y - Z.cols(1, p) * beta.rows(1, p), quant));
+  arma::vec res = Y - Z * beta;
+  updateGauss(Z, res, der, gradOld, tau, n1, h1);
+  beta -= gradOld;
+  arma::vec betaDiff = -gradOld;
+  res -= Z * betaDiff;
+  updateGauss(Z, res, der, gradNew, tau, n1, h1);
+  arma::vec gradDiff = gradNew - gradOld;
+  int ite = 1;
+  while (arma::norm(gradNew, "inf") > tol && ite <= iteMax) {
+    double alpha = 1.0;
+    double cross = arma::as_scalar(betaDiff.t() * gradDiff);
+    if (cross > 0) {
+      double a1 = cross / arma::as_scalar(gradDiff.t() * gradDiff);
+      double a2 = arma::as_scalar(betaDiff.t() * betaDiff) / cross;
+      alpha = std::min(std::min(a1, a2), 100.0);
+    }
+    gradOld = gradNew;
+    betaDiff = -alpha * gradNew;
+    beta += betaDiff;
+    res -= Z * betaDiff;
+    updateGauss(Z, res, der, gradNew, tau, n1, h1);
+    gradDiff = gradNew - gradOld;
+    ite++;
+  }
+  beta.rows(1, p) /= sx;
+  beta(0) += my - arma::as_scalar(mx * beta.rows(1, p));
+  return Rcpp::List::create(Rcpp::Named("coeff") = beta, Rcpp::Named("ite") = ite, Rcpp::Named("residual") = res, Rcpp::Named("bandwidth") = h);
+}
+
+
