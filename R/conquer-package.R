2c91b6495a2406f3f3156ab46c7a4a14996daf3e
Xiaoou Pan <xip024@ucsd.edu>
2021-10-19 13:36:08 -0700

    Update description of package

--- R/conquer-package.R
+++ R/conquer-package.R
@@ -1,11 +1,11 @@
 #' @docType package
 #' @name conquer-package
 #' @title Conquer: Convolution-Type Smoothed Quantile Regression
-#' @description Fast and accurate convolution-type smoothed quantile regression. Implemented using Barzilai-Borwein gradient descent with a Huber regression warm start. Construct confidence intervals for regression coefficients using multiplier bootstrap.
+#' @description Fast and accurate convolution-type smoothed quantile regression. Implemented using Barzilai-Borwein gradient descent with a Huber-type regression warm start. Construct confidence intervals for regression coefficients using multiplier bootstrap.
 #' @author Xuming He <xmhe@umich.edu>, Xiaoou Pan <xip024@ucsd.edu>, Kean Ming Tan <keanming@umich.edu>, and Wen-Xin Zhou <wez243@ucsd.edu>
 #' @references Barzilai, J. and Borwein, J. M. (1988). Two-point step size gradient methods. IMA J. Numer. Anal. 8 141–148.
-#' @references Fernandes, M., Guerre, E. and Horta, E. (2019). Smoothing quantile regressions. J. Bus. Econ. Statist., in press.
-#' @references He, X., Pan, X., Tan, K. M., and Zhou, W.-X. (2020). Smoothed quantile regression for large-scale inference. Preprint.
+#' @references Fernandes, M., Guerre, E. and Horta, E. (2019). Smoothing quantile regressions. J. Bus. Econ. Statist. 39 338-357.
+#' @references He, X., Pan, X., Tan, K. M., and Zhou, W.-X. (2020). Smoothed quantile regression for large-scale inference. J. Econometrics, in press.
 #' @references Horowitz, J. L. (1998). Bootstrap methods for median regression models. Econometrica 66 1327–1351.
 #' @references Koenker, R. (2005). Quantile Regression. Cambridge University Press, Cambridge.
 #' @references Koenker, R. Package "quantreg".
