c61e3d6da25cd692b3766d9efa87790f35d07229
Xiaoou Pan <xip024@ucsd.edu>
2021-10-20 12:09:16 -0700

    Add scad and mcp with Gaussian kernel

--- src/smqr.cpp
+++ src/smqr.cpp
@@ -1097,3 +1097,140 @@ arma::vec lasso(const arma::mat& Z, const arma::vec& Y, const double lambda, con
   }
   return betaNew;
 }
+
+// [[Rcpp::export]]
+double lammSmqrGauss(const arma::mat& Z, const arma::vec& Y, const arma::vec& Lambda, arma::vec& beta, const double phi, const double tau, 
+                     const double gamma, const int p, const double h, const double n1, const double h1, const double h2) {
+  double phiNew = phi;
+  arma::vec betaNew(p + 1);
+  arma::vec grad(p + 1);
+  double loss = updateGaussHd(Z, Y, beta, grad, tau, n1, h, h1, h2);
+  while (true) {
+    arma::vec first = beta - grad / phiNew;
+    arma::vec second = Lambda / phiNew;
+    betaNew = softThresh(first, second, p);
+    double fVal = lossGaussHd(Z, Y, betaNew, tau, h, h1, h2);
+    arma::vec diff = betaNew - beta;
+    double psiVal = loss + arma::as_scalar(grad.t() * diff) + 0.5 * phiNew * arma::as_scalar(diff.t() * diff);
+    if (fVal <= psiVal) {
+      break;
+    }
+    phiNew *= gamma;
+  }
+  beta = betaNew;
+  return phiNew;
+}
+
+// [[Rcpp::export]]
+arma::vec smqrLassoGauss(const arma::mat& Z, const arma::vec& Y, const double lambda, const arma::vec& sx1, const double tau, const int p, const double n1, 
+                         const double h, const double h1, const double h2, const double phi0 = 0.01, const double gamma = 1.2, const double epsilon = 0.001, 
+                         const int iteMax = 500) {
+  arma::vec beta = lasso(Z, Y, lambda, tau, p, n1, phi0, gamma, epsilon, iteMax);
+  arma::vec betaNew = beta;
+  arma::vec Lambda = cmptLambdaLasso(lambda, p);
+  double phi = phi0;
+  int ite = 0;
+  while (ite <= iteMax) {
+    ite++;
+    phi = lammSmqrGauss(Z, Y, Lambda, betaNew, phi, tau, gamma, p, h, n1, h1, h2);
+    phi = std::max(phi0, phi / gamma);
+    if (arma::norm(betaNew - beta, "inf") <= epsilon) {
+      break;
+    }
+    beta = betaNew;
+  }
+  return betaNew;
+}
+
+// [[Rcpp::export]]
+arma::vec smqrScadGauss(const arma::mat& Z, const arma::vec& Y, const double lambda, const arma::vec& sx1, const double tau, const int p, const double n1, 
+                        const double h, const double h1, const double h2, const double phi0 = 0.01, const double gamma = 1.2, const double epsilon = 0.001, 
+                        const int iteMax = 500, const int iteTight = 3, const double para = 3.7) {
+  arma::vec beta = lasso(Z, Y, lambda, tau, p, n1, phi0, gamma, epsilon, iteMax);
+  arma::vec betaNew = beta;
+  // Contraction
+  arma::vec Lambda = cmptLambdaSCAD(beta, lambda, p, para);
+  double phi = phi0;
+  int ite = 0;
+  while (ite <= iteMax) {
+    ite++;
+    phi = lammSmqrGauss(Z, Y, Lambda, betaNew, phi, tau, gamma, p, h, n1, h1, h2);
+    phi = std::max(phi0, phi / gamma);
+    if (arma::norm(betaNew - beta, "inf") <= epsilon) {
+      break;
+    }
+    beta = betaNew;
+  }
+  int iteT = 1;
+  // Tightening
+  arma::vec beta0(p + 1);
+  while (iteT <= iteTight) {
+    iteT++;
+    beta = betaNew;
+    beta0 = betaNew;
+    Lambda = cmptLambdaSCAD(beta, lambda, p, para);
+    phi = phi0;
+    ite = 0;
+    while (ite <= iteMax) {
+      ite++;
+      phi = lammSmqrGauss(Z, Y, Lambda, betaNew, phi, tau, gamma, p, h, n1, h1, h2);
+      phi = std::max(phi0, phi / gamma);
+      if (arma::norm(betaNew - beta, "inf") <= epsilon) {
+        break;
+      }
+      beta = betaNew;
+    }
+    if (arma::norm(betaNew - beta0, "inf") <= epsilon) {
+      break;
+    }
+  }
+  return betaNew;
+}
+
+// [[Rcpp::export]]
+arma::vec smqrMcpGauss(const arma::mat& Z, const arma::vec& Y, const double lambda, const arma::vec& sx1, const double tau, const int p, const double n1, 
+                        const double h, const double h1, const double h2, const double phi0 = 0.01, const double gamma = 1.5, const double epsilon = 0.001, 
+                        const int iteMax = 500, const int iteTight = 3, const double para = 3) {
+  arma::vec beta = lasso(Z, Y, lambda, tau, p, n1, phi0, gamma, epsilon, iteMax);
+  arma::vec betaNew = beta;
+  // Contraction
+  arma::vec Lambda = cmptLambdaMCP(beta, lambda, p, para);
+  double phi = phi0;
+  int ite = 0;
+  while (ite <= iteMax) {
+    ite++;
+    phi = lammSmqrGauss(Z, Y, Lambda, betaNew, phi, tau, gamma, p, h, n1, h1, h2);
+    phi = std::max(phi0, phi / gamma);
+    if (arma::norm(betaNew - beta, "inf") <= epsilon) {
+      break;
+    }
+    beta = betaNew;
+  }
+  int iteT = 1;
+  // Tightening
+  arma::vec beta0(p + 1);
+  while (iteT <= iteTight) {
+    iteT++;
+    beta = betaNew;
+    beta0 = betaNew;
+    Lambda = cmptLambdaMCP(beta, lambda, p, para);
+    phi = phi0;
+    ite = 0;
+    while (ite <= iteMax) {
+      ite++;
+      phi = lammSmqrGauss(Z, Y, Lambda, betaNew, phi, tau, gamma, p, h, n1, h1, h2);
+      phi = std::max(phi0, phi / gamma);
+      if (arma::norm(betaNew - beta, "inf") <= epsilon) {
+        break;
+      }
+      beta = betaNew;
+    }
+    if (arma::norm(betaNew - beta0, "inf") <= epsilon) {
+      break;
+    }
+  }
+  return betaNew;
+}
+
+
+
