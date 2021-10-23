f9a54f129b013ee0c3e7934c68c7aa3ff4a8ba20
Xiaoou Pan <xip024@ucsd.edu>
2021-10-23 00:01:14 -0700

    add conquer process

--- R/smqr.R
+++ R/smqr.R
@@ -45,6 +45,7 @@ getNormCI = function(est, sd, z) {
 #' @references He, X., Pan, X., Tan, K. M., and Zhou, W.-X. (2021+). Smoothed quantile regression for large-scale inference. J. Econometrics, in press.
 #' @references Koenker, R. and Bassett, G. (1978). Regression quantiles. Econometrica 46 33-50.
 #' @author Xuming He <xmhe@umich.edu>, Xiaoou Pan <xip024@ucsd.edu>, Kean Ming Tan <keanming@umich.edu>, and Wen-Xin Zhou <wez243@ucsd.edu>
+#' @seealso See \code{\link{conquer.process}} for smoothed quantile regression process.
 #' @examples 
 #' n = 500; p = 10
 #' beta = rep(1, p)
@@ -131,3 +132,151 @@ conquer = function(X, Y, tau = 0.5, kernel = c("Gaussian", "logistic", "uniform"
                  n = nrow(X), p = ncol(X), perCI = as.matrix(ciList$perCI), pivCI = as.matrix(ciList$pivCI), normCI = normCI))
   }
 }
+
+#' @title Convolution-Type Smoothed Quantile Regression Process
+#' @description Fit a smoothed quantile regression process for a quantile grid. The algorithm is essentially the same as \code{\link{conquer}}.
+#' @param X A \eqn{n} by \eqn{p} design matrix. Each row is a vector of observation with \eqn{p} covariates. Number of observations \eqn{n} must be greater than number of covariates \eqn{p}.
+#' @param Y An \eqn{n}-dimensional response vector.
+#' @param tauSeq (\strong{optional}) The desired quantile grid. Default is {0.1, 0.15, 0.2, ..., 0.85, 0.9}. Values must be between 0 and 1.
+#' @param kernel (\strong{optional})  A character string specifying the choice of kernel function. Default is "Gaussian". Choices are "Gaussian", "logistic", "uniform", "parabolic" or "triangular".
+#' @param h (\strong{optional}) The bandwidth parameter for kernel smoothing. Default is \eqn{max(((log(n) + p) / n)^{0.4}, 0.05)}. The default will be used if the input value is less than 0.05.
+#' @param checkSing (\strong{optional}) A logical flag. Default is FALSE. If \code{checkSing = TRUE}, then it will check if the design matrix is singular before running conquer. 
+#' @param tol (\strong{optional}) Tolerance level of the gradient descent algorithm. The gradient descent algorithm terminates when the maximal entry of the gradient is less than \code{tol}. Default is 1e-04. 
+#' @param iteMax (\strong{optional}) Maximum number of iterations. Default is 5000.
+#' @return An object containing the following items will be returned:
+#' \describe{
+#' \item{\code{coeff}}{A \eqn{(p + 1)} by \eqn{m} matrix of estimated quantile regression process coefficients, including the intercept. m is the length of \code{tauSeq}.}
+#' \item{\code{bandwidth}}{The value of smoothing bandwidth.}
+#' \item{\code{tauSeq}}{The desired quantile levels for the process.}
+#' \item{\code{kernel}}{The choice of kernel function.}
+#' \item{\code{n}}{The sample size.}
+#' \item{\code{p}}{The dimension of the covariates.}
+#' }
+#' @references Barzilai, J. and Borwein, J. M. (1988). Two-point step size gradient methods. IMA J. Numer. Anal. 8 141–148.
+#' @references Fernandes, M., Guerre, E. and Horta, E. (2019). Smoothing quantile regressions. J. Bus. Econ. Statist., in press.
+#' @references He, X., Pan, X., Tan, K. M., and Zhou, W.-X. (2021+). Smoothed quantile regression for large-scale inference. J. Econometrics, in press.
+#' @references Koenker, R. and Bassett, G. (1978). Regression quantiles. Econometrica 46 33-50.
+#' @author Xuming He <xmhe@umich.edu>, Xiaoou Pan <xip024@ucsd.edu>, Kean Ming Tan <keanming@umich.edu>, and Wen-Xin Zhou <wez243@ucsd.edu>
+#' @seealso See \code{\link{conquer}} for single-index smoothed quantile regression.
+#' @examples 
+#' n = 500; p = 10
+#' beta = rep(1, p)
+#' X = matrix(rnorm(n * p), n, p)
+#' Y = 1 + X %*% beta + rt(n, 2)
+#' 
+#' ## Smoothed quantile regression process with Gaussian kernel
+#' fit.Gauss = conquer(X, Y, tauSeq = seq(0.2, 0.8, by = 0.05), kernel = "Gaussian")
+#' beta.hat.Gauss = fit.Gauss$coeff
+#' 
+#' ## Smoothe quantile regression with uniform kernel
+#' fit.unif = conquer(X, Y, tauSeq = seq(0.2, 0.8, by = 0.05), kernel = "uniform")
+#' beta.hat.unif = fit.unif$coeff
+#' @export 
+conquer.process = function(X, Y, tauSeq = seq(0.1, 0.9, by = 0.05), kernel = c("Gaussian", "logistic", "uniform", "parabolic", "triangular"), h = 0.0, checkSing = FALSE, tol = 0.0001, 
+                           iteMax = 5000, ci = FALSE, alpha = 0.05, B = 1000) {
+  if (nrow(X) != length(Y)) {
+    stop("Error: the length of Y must be the same as the number of rows of X.")
+  }
+  if (ncol(X) >= nrow(X)) {
+    stop("Error: the number of columns of X cannot exceed the number of rows of X.")
+  }
+  if(min(tauSeq) <= 0 || max(tauSeq) >= 1) {
+    stop("Error: every quantile level must be in (0, 1).")
+  }
+  if (min(colSds(X)) == 0) {
+    stop("Error: at least one column of X is constant.")
+  }
+  if (checkSing && rankMatrix(X)[1] < ncol(X)) {
+    stop("Error: the design matrix X is singular.")
+  }
+  kernel = match.arg(kernel)
+  rst = NULL
+  if (kernel == "Gaussian") {
+    rst = smqrGaussProc(X, Y, tauSeq, h, tol = tol, iteMax = iteMax)
+  } else if (kernel == "logistic") {
+    rst = smqrLogisticProc(X, Y, tauSeq, h, tol = tol, iteMax = iteMax)
+  } else if (kernel == "uniform") {
+    rst = smqrUnifProc(X, Y, tauSeq, h, tol = tol, iteMax = iteMax)
+  } else if (kernel == "parabolic") {
+    rst = smqrParaProc(X, Y, tauSeq, h, tol = tol, iteMax = iteMax)
+  } else {
+    rst = smqrTrianProc(X, Y, tauSeq, h, tol = tol, iteMax = iteMax)
+  }
+  return (list(coeff = rst$coeff, bandwidth = rst$bandwidth, tauSeq = tauSeq, kernel = kernel, n = nrow(X), p = ncol(X)))
+}
+
+#' @title Convolution-Type Smoothed Quantile Regression Process
+#' @description Fit a smoothed quantile regression process for a quantile grid. The algorithm is essentially the same as \code{\link{conquer}}.
+#' @param X A \eqn{n} by \eqn{p} design matrix. Each row is a vector of observation with \eqn{p} covariates. Number of observations \eqn{n} must be greater than number of covariates \eqn{p}.
+#' @param Y An \eqn{n}-dimensional response vector.
+#' @param tauSeq (\strong{optional}) The desired quantile grid. Default is {0.1, 0.15, 0.2, ..., 0.85, 0.9}. Values must be between 0 and 1.
+#' @param kernel (\strong{optional})  A character string specifying the choice of kernel function. Default is "Gaussian". Choices are "Gaussian", "logistic", "uniform", "parabolic" or "triangular".
+#' @param h (\strong{optional}) The bandwidth parameter for kernel smoothing. Default is \eqn{max(((log(n) + p) / n)^{0.4}, 0.05)}. The default will be used if the input value is less than 0.05.
+#' @param checkSing (\strong{optional}) A logical flag. Default is FALSE. If \code{checkSing = TRUE}, then it will check if the design matrix is singular before running conquer. 
+#' @param tol (\strong{optional}) Tolerance level of the gradient descent algorithm. The gradient descent algorithm terminates when the maximal entry of the gradient is less than \code{tol}. Default is 1e-04. 
+#' @param iteMax (\strong{optional}) Maximum number of iterations. Default is 5000.
+#' @return An object containing the following items will be returned:
+#' \describe{
+#' \item{\code{coeff}}{A \eqn{(p + 1)} by \eqn{m} matrix of estimated quantile regression process coefficients, including the intercept. m is the length of \code{tauSeq}.}
+#' \item{\code{bandwidth}}{The value of smoothing bandwidth.}
+#' \item{\code{tauSeq}}{The desired quantile levels for the process.}
+#' \item{\code{kernel}}{The choice of kernel function.}
+#' \item{\code{n}}{The sample size.}
+#' \item{\code{p}}{The dimension of the covariates.}
+#' }
+#' @references Barzilai, J. and Borwein, J. M. (1988). Two-point step size gradient methods. IMA J. Numer. Anal. 8 141–148.
+#' @references Fernandes, M., Guerre, E. and Horta, E. (2019). Smoothing quantile regressions. J. Bus. Econ. Statist., in press.
+#' @references He, X., Pan, X., Tan, K. M., and Zhou, W.-X. (2021+). Smoothed quantile regression for large-scale inference. J. Econometrics, in press.
+#' @references Koenker, R. and Bassett, G. (1978). Regression quantiles. Econometrica 46 33-50.
+#' @author Xuming He <xmhe@umich.edu>, Xiaoou Pan <xip024@ucsd.edu>, Kean Ming Tan <keanming@umich.edu>, and Wen-Xin Zhou <wez243@ucsd.edu>
+#' @seealso See \code{\link{conquer}} for single-index smoothed quantile regression.
+#' @examples 
+#' n = 500; p = 10
+#' beta = rep(1, p)
+#' X = matrix(rnorm(n * p), n, p)
+#' Y = 1 + X %*% beta + rt(n, 2)
+#' 
+#' ## Smoothed quantile regression process with Gaussian kernel
+#' fit.Gauss = conquer(X, Y, tauSeq = seq(0.2, 0.8, by = 0.05), kernel = "Gaussian")
+#' beta.hat.Gauss = fit.Gauss$coeff
+#' 
+#' ## Smoothe quantile regression with uniform kernel
+#' fit.unif = conquer(X, Y, tauSeq = seq(0.2, 0.8, by = 0.05), kernel = "uniform")
+#' beta.hat.unif = fit.unif$coeff
+#' @export 
+conquer.lasso = function(X, Y, tauSeq = seq(0.1, 0.9, by = 0.05), kernel = c("Gaussian", "logistic", "uniform", "parabolic", "triangular"), h = 0.0, checkSing = FALSE, tol = 0.0001, 
+                         iteMax = 5000, ci = FALSE, alpha = 0.05, B = 1000) {
+  if (nrow(X) != length(Y)) {
+    stop("Error: the length of Y must be the same as the number of rows of X.")
+  }
+  if (ncol(X) >= nrow(X)) {
+    stop("Error: the number of columns of X cannot exceed the number of rows of X.")
+  }
+  if(min(tauSeq) <= 0 || max(tauSeq) >= 1) {
+    stop("Error: every quantile level must be in (0, 1).")
+  }
+  if (min(colSds(X)) == 0) {
+    stop("Error: at least one column of X is constant.")
+  }
+  if (checkSing && rankMatrix(X)[1] < ncol(X)) {
+    stop("Error: the design matrix X is singular.")
+  }
+  kernel = match.arg(kernel)
+  rst = NULL
+  if (kernel == "Gaussian") {
+    rst = smqrGaussProc(X, Y, tauSeq, h, tol = tol, iteMax = iteMax)
+  } else if (kernel == "logistic") {
+    rst = smqrLogisticProc(X, Y, tauSeq, h, tol = tol, iteMax = iteMax)
+  } else if (kernel == "uniform") {
+    rst = smqrUnifProc(X, Y, tauSeq, h, tol = tol, iteMax = iteMax)
+  } else if (kernel == "parabolic") {
+    rst = smqrParaProc(X, Y, tauSeq, h, tol = tol, iteMax = iteMax)
+  } else {
+    rst = smqrTrianProc(X, Y, tauSeq, h, tol = tol, iteMax = iteMax)
+  }
+  return (list(coeff = rst$coeff, bandwidth = rst$bandwidth, tauSeq = tauSeq, kernel = kernel, n = nrow(X), p = ncol(X)))
+}
+
+
+
+
