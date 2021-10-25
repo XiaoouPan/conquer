b7e56c627dbfafd7a7e3806e2a4565f78b72e1b4
Xiaoou Pan <xip024@ucsd.edu>
2021-10-24 19:58:52 -0700

    update package description

--- R/conquer-package.R
+++ R/conquer-package.R
@@ -1,12 +1,13 @@
 #' @docType package
 #' @name conquer-package
 #' @title Conquer: Convolution-Type Smoothed Quantile Regression
-#' @description Fast and accurate convolution-type smoothed quantile regression. Implemented using Barzilai-Borwein gradient descent with a Huber-type regression warm start for low-dimensional setting, and a local majorize-minimize algorithm for high-dimensional setting. Construct confidence intervals for regression coefficients using multiplier bootstrap.
+#' @description Fast and accurate convolution-type smoothed quantile regression for low/high-dimensional estimation and inference.
 #' @author Xuming He <xmhe@umich.edu>, Xiaoou Pan <xip024@ucsd.edu>, Kean Ming Tan <keanming@umich.edu>, and Wen-Xin Zhou <wez243@ucsd.edu>
 #' @references Barzilai, J. and Borwein, J. M. (1988). Two-point step size gradient methods. IMA J. Numer. Anal. 8 141–148.
+#' @references Belloni, A. and Chernozhukov, V. (2011). l_1 penalized quantile regression in high-dimensional sparse models. Ann. Statist. 39 82-130.
+#' @references Fan, J., Liu, H., Sun, Q. and Zhang, T. (2018). I-LAMM for sparse learning: Simultaneous control of algorithmic complexity and statistical error. Ann. Statist. 46 814-841.
 #' @references Fernandes, M., Guerre, E. and Horta, E. (2019). Smoothing quantile regressions. J. Bus. Econ. Statist. 39 338-357.
 #' @references He, X., Pan, X., Tan, K. M., and Zhou, W.-X. (2021+). Smoothed quantile regression for large-scale inference. J. Econometrics, in press.
-#' @references He, X., Pan, X., Tan, K. M., and Zhou, W.-X. (2021). Scalable Estimation and Inference for Censored Quantile Regression Process. Preprint.
 #' @references Horowitz, J. L. (1998). Bootstrap methods for median regression models. Econometrica 66 1327–1351.
 #' @references Koenker, R. (2005). Quantile Regression. Cambridge University Press, Cambridge.
 #' @references Koenker, R. Package "quantreg".
