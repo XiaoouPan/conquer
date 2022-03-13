0174ef5c7cc2833cedcb20bc4288459e47063407
XiaoouPan <xip024@ucsd.edu>
2022-03-13 13:43:17 -0700

    header file for basic oparations

---
+++ src/basicOp.h
@@ -0,0 +1,28 @@
+#ifndef BASICOP_H
+#define BASICOP_H
+
+# include <RcppArmadillo.h>
+# include <cmath>
+// [[Rcpp::depends(RcppArmadillo)]]
+// [[Rcpp::plugins(cpp11)]]
+
+// [[Rcpp::export]]
+int sgn(const double x) {
+  return (x > 0) - (x < 0);
+}
+
+// [[Rcpp::export]]
+arma::mat standardize(arma::mat X, const arma::rowvec& mx, const arma::vec& sx1, const int p) {
+  for (int i = 0; i < p; i++) {
+    X.col(i) = (X.col(i) - mx(i)) * sx1(i);
+  }
+  return X;
+}
+
+// [[Rcpp::export]]
+arma::vec softThresh(const arma::vec& x, const arma::vec& Lambda, const int p) {
+  return arma::sign(x) % arma::max(arma::abs(x) - Lambda, arma::zeros(p + 1));
+}
+
+
+#endif
